8c2f455d422c67eadd4ae7176efa9909be8f1a6d
martcoder <58665955+martcoder@users.noreply.github.com>
2022-03-16 14:28:37 +0000

    Coding the motion model currently

--- state_estimation/simulation.py
+++ state_estimation/simulation.py
@@ -106,3 +106,77 @@ for filename in filenamesLidar:
    if filename == str("Accel15psi.datavarholder.datavarhistogram.data"):
      LidarmeasurementModel['15psi'][keyval[0]] = keyval[1]
 
+#Now for the motion models
+state_probability = 0.1; # 10 possible states, 1/10
+motion_model_60psi = dict()
+motion_model_60psi[15] = 0.005
+motion_model_60psi[20] = 0.005
+motion_model_60psi[25] = 0.01
+motion_model_60psi[30] = 0.02
+motion_model_60psi[35] = 0.02
+motion_model_60psi[40] = 0.02
+motion_model_60psi[45] = 0.02
+motion_model_60psi[50] = 0.2
+motion_model_60psi[55] = 0.3
+motion_model_60psi[60] = 0.4
+
+motion_model_55psi = dict()
+motion_model_55psi[15] = 0.005
+motion_model_55psi[20] = 0.005
+motion_model_55psi[25] = 0.01
+motion_model_55psi[30] = 0.02
+motion_model_55psi[35] = 0.02
+motion_model_55psi[40] = 0.02
+motion_model_55psi[45] = 0.02
+motion_model_55psi[50] = 0.3
+motion_model_55psi[55] = 0.5
+motion_model_55psi[60] = 0.1
+
+motion_model_50psi = dict()
+motion_model_50psi[15] = 0.005
+motion_model_50psi[20] = 0.005
+motion_model_50psi[25] = 0.02
+motion_model_50psi[30] = 0.02
+motion_model_50psi[35] = 0.02
+motion_model_50psi[40] = 0.02
+motion_model_50psi[45] = 0.3
+motion_model_50psi[50] = 0.5
+motion_model_50psi[55] = 0.1
+motion_model_50psi[60] = 0.01
+
+motion_model_45psi = dict()
+motion_model_45psi[15] = 0.01
+motion_model_45psi[20] = 0.02
+motion_model_45psi[25] = 0.02
+motion_model_45psi[30] = 0.02
+motion_model_45psi[35] = 0.02
+motion_model_45psi[40] = 0.3
+motion_model_45psi[45] = 0.4
+motion_model_45psi[50] = 0.2
+motion_model_45psi[55] = 0.005
+motion_model_45psi[60] = 0.005
+
+motion_model_40psi = dict()
+motion_model_40psi[15] = 0.01
+motion_model_40psi[20] = 0.02
+motion_model_40psi[25] = 0.02
+motion_model_40psi[30] = 0.02
+motion_model_40psi[35] = 0.3
+motion_model_40psi[40] = 0.3
+motion_model_40psi[45] = 0.3
+motion_model_40psi[50] = 0.02
+motion_model_40psi[55] = 0.005
+motion_model_40psi[60] = 0.005
+
+motion_model_35psi = dict()
+motion_model_35psi[15] = 0.01
+motion_model_35psi[20] = 0.02
+motion_model_35psi[25] = 0.02
+motion_model_35psi[30] = 0.3
+motion_model_35psi[35] = 0.5
+motion_model_35psi[40] = 0.1
+motion_model_35psi[45] = 0.02
+motion_model_35psi[50] = 0.02
+motion_model_35psi[55] = 0.005
+motion_model_35psi[60] = 0.005
+
