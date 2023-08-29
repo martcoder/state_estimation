a255939f20645639ddec5ee383e0e1618e1aac60
martcoder <martcoder@martcoder.github>
2023-08-29 22:24:15 +0100

    Decided to shift the code to C in order to make it more robust.
    Python is giving some weird results, so moving the code to C in
    order to have better control of what's happening. Also C should
    run faster if I can get it to work. Will take a bit of time,
    takes longer to code in C.

---
+++ ffann/ffann_aux.c
@@ -0,0 +1,417 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#https://medium.com/@b.terryjack/introduction-to-deep-learning-feed-forward-neural-networks-ffnns-a-k-a-c688d83a309d 
+#https://en.wikipedia.org/wiki/Tournament_selection
+#https://en.wikipedia.org/wiki/Crossover_(genetic_algorithm)
+
+#Before running, ensure you edit script to include the data filenames you want to be processed
+#... they are just a few lines down from here....
+#run as python3 ffann.py accel | lidar
+# e.g. python3 ffan.py accel
+#EDIT: The expected results are now hard-coded
+#... into the process function! So if you want to change them they needed editing there
+
+typedef struct node {
+	float input;
+	float weight; // initialise using ((float)rand()/(float)(RAND_MAX)) * upperLimit ... ref:stackoverflow.com/questions/13408990 
+	float bias;
+	float * weights;
+	float output;
+	float lms; //initialse to highh value
+}Node;
+
+typedef struct individual {
+	Node * inputLayer;
+	int numberOfHiddenNodes;
+	Node * hiddenLayer;
+	int numberOfOutputNodes;
+	Node * outputLayer;
+	float best;
+	float lms;
+	float lmsLOW;
+	float lmsMED;
+	float lmsHIGH;
+}Individual;
+
+typedef struct population {
+	Individual * oldpopulation;
+	Individual * newpopulation;		
+}Population;
+
+int numCycles;
+int nodeSizeMemory;
+int individualSizeMemory;
+int defaultNumberOutputNodes;
+
+Population population;
+
+char * filenamesList[3][1024]; //3 arrays of strings
+char filenamesListLow[3][1024]; //3 strings
+char filenamesListMiddle[4][1024]; //4 strings
+char filenamesListHigh[3][1024]; //3 strings
+int chosenSensor; //0 is for accelerometer, 1 is for lidar
+
+
+
+global result 
+result = []
+global resultLOW
+resultLOW = []
+global resultMED
+resultMED = []
+global resultHIGH
+resultHIGH = []
+global lmsResult
+lmsResult = []
+global LMSresultLOW
+LMSresultLOW = []
+global LMSresultMED
+LMSresultMED = []
+global LMSresultHIGH
+LMSresultHIGH = []
+
+
+intendedResult = sys.argv[1]
+global bestlms 
+bestlms= 1000000000000000000.0 # assigning initial high value
+
+
+global popsize 
+popsize = 4
+global hiddenMax
+hiddenMax = 20
+hiddenMin = 5
+outputLength = 3
+global weightMax
+weightMax = 2.0
+global elitism
+elitism = max(1, math.ceil( popsize / 10.0 ) )
+
+
+
+#set useful variables which define structure of ANNs and hold final best coefficients
+global bestInputLayer
+bestInputLayer = Node()
+global bestHiddenLayer
+bestHiddenLayer = []
+global bestOutputLayer
+bestOutputLayer = []
+
+global inputLayer
+inputLayer = Node()
+global hiddenLayer
+hiddenLayer = []
+#hiddenLayer.append(Node(2))
+#hiddenLayer.append(Node(3))
+global outputLayer
+outputLayer = []
+
+#Activation functions: https://www.geeksforgeeks.org/activation-functions-neural-networks/
+def sigmoid(value):
+  return 1.0 / (1.0 + math.exp( (-1.0) * value) )
+
+def relu(value):
+  return max(0.0,abs(value)) #accel data has plenty of negative values, so using absolute
+
+def process(filenamesList,expectedResult,member):
+ global global_population #need this global keyword to access global object !!!
+ r = 0 #for choosing the expected result, will increment at the end of the following for loop
+ for filenamearray in filenamesList:
+  if r == 0:
+    expectedResult = 5100 # LOW pressure data being trained with
+  if r == 1:
+    expectedResult = 5010 # MED pressure data being trained with
+  if r == 2:
+    expectedResult = 5001 # HIGH pressure data being trained with
+
+  for name in filenamearray:
+   #print('processing datafile '+name)
+   filehold = open(name,"r")
+   Lines = filehold.readlines()
+   for x in Lines:
+     splitLine = x.split(',')
+     value = splitLine[0]
+     floatval = float(value)
+     if chosenSensor == 'lidar':
+       if (floatval) < 400.0 or (floatval > 650.0): #filter outliers
+          floatval = 500.0 #Remove outlier and just use regular value
+     #print("Population member number is "+str(member))
+     #print("Population input weight is "+str(oldpopulation[member].inputLayer.weight))
+     #print("Input value stored in this member is currently "+str(oldpopulation[member].inputLayer.input) )
+     global_population.oldpopulation[member].inputLayer.input = floatval
+
+     #processing input node
+     global_population.oldpopulation[member].inputLayer.output = global_population.oldpopulation[member].inputLayer.input * global_population.oldpopulation[member].inputLayer.weight # multiply input by weight
+     global_population.oldpopulation[member].inputLayer.output = global_population.oldpopulation[member].inputLayer.output + global_population.oldpopulation[member].inputLayer.bias # add the bias into the mix
+     global_population.oldpopulation[member].inputLayer.output = relu(global_population.oldpopulation[member].inputLayer.output) # run through activation func
+     for h in global_population.oldpopulation[member].hiddenLayer:
+       h.output = global_population.oldpopulation[member].inputLayer.output * h.weight
+       h.output = h.output + h.bias
+       h.output = relu(h.output)
+     #now process the output node
+     #oldpopulation[member].outputLayer.output = 0.0
+     normalisingList = []
+     for oi in range(len(global_population.oldpopulation[member].outputLayer)):
+        global_population.oldpopulation[member].outputLayer[oi].output = 0.0 #first set to zero for fresh numbers
+        for h in range(len(global_population.oldpopulation[member].hiddenLayer)):
+	  #print("member number "+str(member)+" and h number "+str(h)+" and popsize is "+str(len(oldpopulation))+" and hidden len is "+str(len(oldpopulation[member].hiddenLayer))+" and weights len is "+str(len(oldpopulation[member].outputLayer.weights )))
+           global_population.oldpopulation[member].outputLayer[oi].output += global_population.oldpopulation[member].hiddenLayer[h].output * global_population.oldpopulation[member].outputLayer[oi].weights[h]
+        global_population.oldpopulation[member].outputLayer[oi].output += global_population.oldpopulation[member].outputLayer[oi].bias
+        global_population.oldpopulation[member].outputLayer[oi].output = relu(global_population.oldpopulation[member].outputLayer[oi].output)
+        #print("Final output is "+str(global_population.oldpopulation[member].outputLayer[oi].output))
+        normalisingList.append( global_population.oldpopulation[member].outputLayer[oi].output ) 
+     #print("result is "+str(outputLayer.output))
+     try:
+       largestOutput = max(normalisingList)
+     except ValueError:
+       print("normalisingList seems to not be populated")
+       print("first output node value is "+str(global_population.oldpopulation[0].outputLayer[0].output)) 
+     #print("largest output is "+str(largestOutput))
+     indexOfLargestOutput = normalisingList.index(max(normalisingList))
+
+      #Populise a normalised version of the outputs...
+     normalisedOutput = []
+     normalisedOutput.clear()
+     secondLargestIndex = 0
+     for n in range(len(normalisingList)):
+       normalisedOutput.append( normalisingList[n] / largestOutput )
+       #print("NormalisedOutput "+str(n)+":"+str(normalisedOutput[n]))
+       if( (normalisingList[n] < largestOutput) and (normalisingList[n] > normalisingList[secondLargestIndex]) ):
+         secondLargestIndex = n #update which index contains the 2nd largest number
+
+     
+     #resultLOW.append( oldpopulation[member].outputLayer[0].output )
+     #expectedResultMED = 40.0
+     #resultMED.append( oldpopulation[member].outputLayer[1].output )
+
+     #expectedResultHIGH = 60.0
+     #resultHIGH.append( oldpopulation[member].outputLayer[2].output  )
+     #LMSresultLOW.append( (float(expectedResult) - oldpopulation[member].outputLayer.output )  )
+     #LMSresultMED.append(   )
+
+     if expectedResult == 5001: #HIGH PRESSURE
+        if(indexOfLargestOutput != 2): #Nowhere near where want to be, so....
+          lms = 2 #automatic penalty
+        else: # This is good news, so  action this....
+          # by minusing 1 - 2ndLargest this will produce large LMS when 2ndlargest is large, and small LMS when 2ndlargest is small,
+          # and as small LMS is better, this will punish a large 2ndlargest value :D
+          lms = 1 - (1 - normalisedOutput[secondLargestIndex] )
+        #print("Normalised output values for HIGH pressure data are LOW-MED-HIGH: "+str(normalisedOutput[0])+"-"+str(normalisedOutput[1])+"-"+str(normalisedOutput[0]))
+     elif expectedResult == 5010: #MED PRESSURE
+        if indexOfLargestOutput != 1:
+          lms = 2 # automatic punishment
+        else: 
+          lms = 1 - (1 - normalisedOutput[secondLargestIndex] )
+        #print("Normalised output values for MED pressure data are LOW-MED-HIGH: "+str(normalisedOutput[0])+"-"+str(normalisedOutput[1])+"-"+str(normalisedOutput[0]))
+     elif expectedResult == 5100: #LOW PRESSURE
+       if indexOfLargestOutput != 0:
+          lms = 2
+       else:
+          lms = 1 - (1 - normalisedOutput[secondLargestIndex] ) 
+       #print("Normalised output values for LOW pressure data are LOW-MED-HIGH: "+str(normalisedOutput[0])+"-"+str(normalisedOutput[1])+"-"+str(normalisedOutput[0]))
+     else:
+       print("Hmmm the expected result was not one of the usual ones...")
+
+     #lms = (float(expectedResult) - oldpopulation[member].outputLayer.output )
+     lms = lms * lms
+     lmsResult.append( lms )
+  r += 1 #increment which expected result needs using
+ print("Member "+str(member)+" has processed low med and high data and has avg lms of "+str(statistics.mean(lmsResult)))
+ global_population.oldpopulation[member].inputLayer.lms = statistics.mean(lmsResult)
+
+
+def addElite():
+  global_population.newpopulation.append(Individual(bestInputLayer,bestHiddenLayer,bestOutputLayer))
+
+
+def tournament():
+  tournySet = []
+  # Select 4 random individuals. 
+  indv = random.randint(0,len(global_population.oldpopulation)-1)
+  
+  #print("old pop size is "+str(len(oldpopulation))+" and index chosen "+str(indv))
+  tournySet.append( global_population.oldpopulation[indv]  )
+  indv = random.randint(1,len(global_population.oldpopulation)-1)
+  #print("old pop size is "+str(len(oldpopulation))+" and index chosen "+str(indv))
+
+  tournySet.append( global_population.oldpopulation[indv]  )
+  indv = random.randint(1,len(global_population.oldpopulation)-1)
+  #print("old pop size is "+str(len(oldpopulation))+" and index chosen "+str(indv))
+
+  tournySet.append( global_population.oldpopulation[indv]  )
+  indv = random.randint(1,len(global_population.oldpopulation)-1)
+  #print("old pop size is "+str(len(oldpopulation))+" and index chosen "+str(indv))
+
+  tournySet.append( global_population.oldpopulation[indv]  )
+
+  #choose 2 best to parent
+  twoParent = []
+  twoParent.append(tournySet[0])
+  twoParent.append(tournySet[1])
+  if tournySet[2].inputLayer.lms < twoParent[0].inputLayer.lms:
+    twoParent[0] = tournySet[2]
+  elif tournySet[2].inputLayer.lms < twoParent[1].inputLayer.lms:
+    twoParent[1] = tournySet[2]
+  if tournySet[3].inputLayer.lms < twoParent[0].inputLayer.lms:
+    twoParent[0] = tournySet[3]
+  elif tournySet[3].inputLayer.lms < twoParent[1].inputLayer.lms:
+    twoParent[1] = tournySet[3]
+
+  #now do breeding
+  newinput = Node()
+  newhidden = []
+  newoutput = []
+  #choose whether to crossover
+  crossover = random.random()
+  if crossover > 0.9:
+   newinput = copy.deepcopy(twoParent[0].inputLayer) # just keep a good parent
+   newhidden = copy.deepcopy(twoParent[0].hiddenLayer)
+   newoutput = copy.deepcopy(twoParent[0].outputLayer)
+  else: #if crossover <= 0.9 then DO CROSSOVER, so the majority of the time
+
+    #choose which parent to get input details from
+    parentInputNode = random.random()
+    if(parentInputNode <= 0.5):
+      newinput.weight = twoParent[0].inputLayer.weight
+      newinput.bias = twoParent[0].inputLayer.bias
+    else:
+      newinput.weight = twoParent[1].inputLayer.weight
+      newinput.bias = twoParent[1].inputLayer.bias 
+    #take half of hidden from par0, half from par1
+    #newhidden = []
+    lenP0 = len(twoParent[0].hiddenLayer)
+    lenP1 = len(twoParent[1].hiddenLayer)
+
+    newoutput.append( Node() )
+    newoutput.append( Node() )
+    newoutput.append( Node() )
+
+    for x in range(int(math.ceil(lenP0/2))): #cycle through 1/2 parent
+      newhidden.append(copy.deepcopy(twoParent[0].hiddenLayer[x]) )
+    newoutput[0].weights = twoParent[0].outputLayer[0].weights 
+      #for w in range(len(twoParent[0].outputLayer)): #cycle through 3 outputs
+      #  newoutputLayer[w].weights.append( twoParent[0].outputLayer[w].weights[x] )
+    for x in range(int(math.ceil(lenP1/2))): #cycle through 1/2 parent
+      newhidden.append( copy.deepcopy(twoParent[1].hiddenLayer[x]) )
+    newoutput[1].weights = twoParent[1].outputLayer[1].weights
+    newoutput[2].weights = twoParent[1].outputLayer[2].weights
+      #for w in range(len(twoParent[1].outputLayer)): #cycle through 3 outputs
+      #  newoutputLayer[w].weights.append(twoParent[0].outputLayer[w].weights[x])
+
+    #now truncate so not too huge....
+
+    if len(newhidden) > (hiddenMax+1):
+       newhidden = newhidden[0:hiddenMax]
+    print("newhidden length is "+str(len(newhidden)))
+    for x in range(len(newoutput)):
+      if len(newoutput[x].weights) > (hiddenMax+1):
+         newoutput[x].weights = newoutput[x].weights[0:hiddenMax] 
+    #take output bias based on previous prob
+    if(parentInputNode <= 0.5):
+       for x in range(3):
+         newoutput[x].bias = twoParent[0].outputLayer[x].bias
+    else:
+       for x in range(3):
+         newoutput[x].bias = twoParent[1].outputLayer[x].bias
+  
+
+  #Now do random mutation
+  doMutationInput = random.random()
+  if(doMutationInput < 0.3):
+    newinput.weight = random.uniform(0.0,weightMax)
+    newinput.bias = random.uniform(0.0,weightMax) #https://stackoverflow.com/questions/6088077/how-to-get-a-random-number-between-a-float-range
+  doMutationHidden = random.random()
+  if(doMutationHidden < 0.3):
+    for x in newhidden:
+      x.weight = random.uniform(0.0,weightMax)
+      x.bias = random.uniform(0.0,weightMax)
+  doMutationOutput = random.random()
+  #choose a random weight index
+  #weightChoice = random.randint(0,len(newoutput[0].weights)-1) #arbitrary choice of first output node for weights length
+  #print("weight number for output node is "+str(weightChoice))
+  if doMutationOutput < 0.3:
+    for x in range(len(newoutput)):
+      weightChoice = random.randint(0,len(newoutput[x].weights)-1)
+      newoutput[x].weights[weightChoice] = random.uniform(0.0,weightMax) #mutate that chosen weight
+      newoutput[x].bias = random.uniform(0.0,weightMax) #also mutate bias
+
+
+
+  #Now add the newly minted individual to the new population
+  global_population.newpopulation.append(Individual(newinput,newhidden,newoutput))
+  print("new populatino size is in tournament now "+str(len(global_population.newpopulation)))
+
+
+
+void constructFFANN(float* oldpopulation):
+ inputLayer = Node() 
+ numberOfHidden = random.randint(hiddenMin,hiddenMax)
+ #print("number of hidden: "+str(numberOfHidden))
+
+ #construct hidden layer
+ hiddenLayer = []
+ for x in range(numberOfHidden):
+   hiddenLayer.append(Node())
+ for x in range(numberOfHidden):
+   hiddenLayer[x].output = 0.0
+   hiddenLayer[x].weight = random.uniform(0.0,weightMax)
+   #hiddenLayer[x].weights.append( random.random()  ) #initialise hidden node weights, in this case just 1 weight...
+
+ #print("length of hidden layer inside constructFFANN :"+str(len(hiddenLayer)))
+ outputLayer = [] #3 nodes, one per expected output
+ outputLayer.append( Node() )
+ outputLayer.append( Node() )
+ outputLayer.append( Node() )
+ #now create output node weights, the same amount as there are hidden nodes
+ for o in outputLayer:
+   o.weights = []
+   o.weights.clear()
+   for x in range( len(hiddenLayer) ): #need same number of weights in each output node as there are hidden nodes...
+     o.weights.append( random.uniform(0.0,weightMax) ) #initialise weights randomly
+
+ #Now add to the current population list
+ 
+ global_population.oldpopulation.append(Individual(inputLayer,hiddenLayer,outputLayer))
+ #print("number of hidden nodes : "+str(len(hiddenLayer)))
+ inputLayer = None
+ hiddenLayer = []
+ outputLayer = []
+
+void constructNode(Node * nodestruct){
+	nodestruct->input = 0.0f;
+	nodestruct->weight = getRandomWeightValueFloat();
+	nodestruct->bias = 0.0f;
+	nodestruct->weights = (float *) malloc( sizeof(float) * hiddenMax );
+	nodestruct->output = 0.0f;
+	nodestruct->lms = 2.0f; // initialising to a high value...
+}
+
+void constructIndividual(Individual * individualstruct){
+  individualstruct->numberOfHiddenNodes = getRandomNumberHiddenNodesInt();
+  individualstruct->numberOfOutputNodes = defaultNumberOutputNodes; 
+	individualstruct->inputLayer = (Node *) malloc(nodeSizeMemory);
+	individualstruct->hiddenLayer = (Node *) malloc(nodeSizeMemory * individualstruct->numberOfHiddenNodes);
+	individualstruct->outputLayer = (Node *) malloc(nodeSizeMemory * individualstruct->numberOfOutputNodes);
+	individualstruct->best = 0.0f;
+	individualstruct->lms = 4.0f;
+	individualstruct->lmsLOW = 4.0f;
+	individualstruct->lmsMED = 4.0f;
+	individualstruct->lmsHIGH = 4.0f;
+}
+
+void constructPopulation(Population * populationstruct){
+  populationstruct->oldpopulation = (Individual*) malloc( individualSizeMemory );
+	populationstruct->newpopulation = (Individual*) malloc( individualSizeMemory );
+}
+
+int getRandomNumberHiddenNodesInt(){
+	return (rand() % (hiddenMax - hiddenMin + 1)) + 1;
+}
+
+float getRandomWeightValueFloat(){
+	return ((float)rand()/(float)(RAND_MAX)) * weightMax; //... ref:stackoverflow.com/questions/13408990 
+}
+
---
+++ ffann/ffann_v2.c
@@ -0,0 +1,147 @@
+
+//from ffann_aux import * #imports global variables, Node and Individual classes
+
+
+int main(int argc, char ** argv){
+	
+	srand((unsigned int)time(NULL)); // needed for random number generation...
+	
+	//Set global variables values
+	numCycles = 50 //global variable
+	nodeSizeMemory = ( sizeof(float) * 5 ) + ( sizeof(float) * hiddenMax );
+	individualSizeMemory = popsize * ( nodeSizeMemory + (hiddenMax * nodeSizeMemory) + (nodeSizeMemory * outputLength) + (sizeof(float) * 5) );
+	defaultNumberOutputNodes = 3;
+	constructPopulation(&population);
+	
+	if(argv[1] == 'a'){
+		chosenSensor = 0; // 0 is for accelerometer, 1 is for lidar. 
+		strcpy(filenamesListLow[0],"Accel15psi.data"); strcpy(filenamesListLow[1],"Accel20psi.data"); strcpy(filenamesListLow[2],"Accel25psi.data");
+		strcpy(filenamesListMiddle[0],"Accel30psi.data"); strcpy(filenamesListMiddle[1],"Accel35psi.data"); strcpy(filenamesListMiddle[2],"Accel40psi.data"); strcpy(filenamesListMiddle[3],"Accel45psi.data");
+		strcpy(filenamesListHigh[0],"Accel50psi.data"); strcpy(filenamesListHigh[1],"Accel55psi.data"); strcpy(filenamesListHigh[2],"Accel60psi.data");
+	}
+	else if( argv[1] == 'l'){
+		chosenSensor = 1; // 1 for lidar
+		strcpy(filenamesListLow[0],"Lidar15psi.data"); strcpy(filenamesListLow[1],"Lidar20psi.data"); strcpy(filenamesListLow[2],"Lidar25psi.data");
+		strcpy(filenamesListMiddle[0],"Lidar30psi.data"); strcpy(filenamesListMiddle[1],"Lidar35psi.data"); strcpy(filenamesListMiddle[2],"Lidar40psi.data"); strcpy(filenamesListMiddle[3],"Lidar45psi.data");
+		strcpy(filenamesListHigh[0],"Lidar50psi.data"); strcpy(filenamesListHigh[1],"Lidar55psi.data"); strcpy(filenamesListHigh[2],"Lidar60psi.data");
+	}
+	else{
+		printf("You need to specify accel or lidar as the argument when running this script");
+		return 1;
+	}
+	
+	filenamesList[0] = filenamesListLow;
+	filenamesList[1] = filenamesListMiddle;
+	filenamesList[2] = filenamesListHigh;
+	
+	
+
+ int c = 0;
+ for( c=0; c < numCycles; c++)
+	
+	  constructFFANN() # create initial population
+
+  for t in range(50): # number of cycles of this evolutionary algorithm
+	  #Process the input data through each population member
+
+	  for x in range(popsize): #e.g. for each member FFANN, process it
+
+		#print("lenght of hidden layer is "+str(len(hiddenLayer)))
+		print("cycle is "+str(t)+", just about to process member "+str(x))    
+		#Run through each line of data in datafile
+		process(filenamesList, intendedResult, x) # filename, intendedResult is redundant, number of member, func populates result list
+
+		#Get datafile result as LeastMeanSquared
+		lmsavg = statistics.mean(lmsResult)
+		#print("lmssum is "+str(lmssum))
+		if lmsavg < bestlms: #Set this ffann as the best so far....
+		  print("Found new best lms of "+str(lmsavg))
+		  #meanResult = statistics.mean(result)
+		  #print("And mean output was "+str(meanResult) )
+		  #meanLOW = statistics.mean(resultLOW)
+		  #meanMED = statistics.mean(resultMED)
+		  #meanHIGH = statistics.mean(resultHIGH)
+		  #print("mean LOW was "+str(meanLOW)+", mean MED was "+str(meanMED)+", mean HIGH was "+str(meanHIGH))
+		  bestInputLayer = copy.deepcopy(global_population.oldpopulation[x].inputLayer)
+		  #bestInputLayer.meanOutput = meanResult
+		  #bestInputLayer.meanOutputLOW = meanLOW
+		  #bestInputLayer.meanOutputMED = meanMED
+		  #bestInputLayer.meanOutputHIGH = meanHIGH
+		  #print("len of hidden layer is "+str(len(hiddenLayer)))
+		  bestHiddenLayer = copy.deepcopy(global_population.oldpopulation[x].hiddenLayer)
+		  bestOutputLayer = copy.deepcopy(global_population.oldpopulation[x].outputLayer)
+		  bestlms = lmsavg
+		lmsResult.clear()  # empty this ready for the next FFANN
+		result = []
+		resultMED = []
+		resultLOW = []
+		resultHIGH = []
+		#lmssum = 0.0
+		#Now make new ffann....cleaning up the previous one
+		inputLayer = Node()
+		hiddenLayer = []
+		hiddenLayer.clear()
+		outputLayer = []
+		outputLayer.clear()
+
+	  #Now CREATE NEW POPULATION
+
+	  global_population.newpopulation = []
+	  #Firstly do elitism
+	  countElite = 0
+	  for x in range(popsize):
+		print("about to add memer to newpop")
+		if countElite < elitism: #elitism num defined at beginning of script
+		  addElite()
+		  countElite += 1
+		  print("Added an elite, total elite to add is "+str(elitism))
+		else:
+		  if len(global_population.newpopulation) < popsize:
+			print("about to tournament")
+			tournament() #to construct new population member
+	  countElite = 0
+	  #oldpopulation.clear()
+	  
+	  global_population.oldpopulation = []
+	  print("New population contains "+str(len(global_population.newpopulation))+" members")
+	  print("Old pop has been cleared and contains "+str(len(global_population.oldpopulation))+" members. Now copying new to old")
+	  #for i in range(len(newpopulation)):
+	  global_population.oldpopulation = copy.deepcopy(global_population.newpopulation) # now copy new population to old population
+	  global_population.newpopulation = [] #.clear()
+	  #newpopulation.clear()
+	#Finally print and save the best FFANN....
+	  print("New pop copied and cleared, now has "+str(len(global_population.newpopulation))+" members. Current population (old) contains "+str(len(global_population.oldpopulation))+" individuals\n")
+
+	  print("..for example oldpop first node has input weight of "+str(global_population.oldpopulation[0].inputLayer.weight) )
+	  print("..and oldpop first node has input value of "+str(global_population.oldpopulation[0].inputLayer.input) )
+
+	  writer = open(str(datetime.now())+"_"+str(intendedResult)+".log","a")
+	  writer.write("The best FFANN for "+str(intendedResult)+" with an lms of "+str(bestInputLayer.lms)+" is:\n")
+
+	  writer.write("Input weight of "+str(bestInputLayer.weight)+" and bias is "+str(bestInputLayer.bias)+"\n")
+	  for x in bestHiddenLayer:
+	   writer.write("Hidden layer node, weight is "+str(x.weight)+" and bias is "+str(x.bias)+"\n")
+	  #for m in bestOutputLayer:
+	  for x in range(3):
+		for w in bestOutputLayer[x].weights:
+		  writer.write("Best output layer Node "+str(x)+" weight: "+str(w)+"\n ")
+		writer.write("output bias for node "+str(x)+" is "+str(bestOutputLayer[x].bias))
+	  writer.close()
+
+	  print("The best FFANN for "+str(intendedResult)+" with an lms of "+str(bestInputLayer.lms)+" is:\n")
+
+	  print("Input weight of "+str(bestInputLayer.weight)+" and bias is "+str(bestInputLayer.bias)+"\n")
+	  for x in bestHiddenLayer:
+	   print("Hidden layer node, weight is "+str(x.weight)+" and bias is "+str(x.bias)+"\n")
+
+	  #for e in bestOutputLayer:
+	  i = 0
+	  for x in bestOutputLayer:
+		for w in x.weights:
+		  print("Best output layer Node "+str(i)+" weight is "+str(w)+"\n ")
+		i = i + 1
+		print("output bias is "+str(x.bias))
+		
+  }
+}
+
